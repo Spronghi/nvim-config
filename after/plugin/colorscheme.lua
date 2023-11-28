9ed51669bdcefe3d9e7b8cfaf428614fedbe3d17
Simone Colaci <simone.colaci@nl.abnamro.com>
2023-11-28 10:32:13 +0100

    changed lualine filename format

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -3,5 +3,7 @@ require('ayu').colorscheme()
 require('lualine').setup({
   options = {
     theme = 'ayu',
+    'filename',
+    path = 3,
   },
 })
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,20 +2,20 @@ local builtin = require('telescope.builtin')
 local telescope = require('telescope')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>hf', function() 
-	builtin.help_tags()
+vim.keymap.set('n', '<leader>hf', function()
+  builtin.help_tags()
 end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pg', function()
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
-telescope.setup{
-	defaults= {
-		file_ignore_patterns = {
-			"node_modules",
-			".git/"
-		}
-	}
+telescope.setup {
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules",
+      ".git/"
+    }
+  }
 }
