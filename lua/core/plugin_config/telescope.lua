77944b0d8f1a2a18b2764fc51b1ccb6ecccf6104
Simone Colaci <simone.colaci@nl.abnamro.com>
2024-01-05 15:34:22 +0100

    cleaned config a bit

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -2,15 +2,10 @@ local builtin = require('telescope.builtin')
 local telescope = require('telescope')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>hf', function()
-  builtin.help_tags()
-end)
+vim.keymap.set('n', '<leader>hf', builtin.help_tags, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pd', builtin.lsp_document_symbols, {})
-vim.keymap.set('n', '<leader>pg', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
 
 telescope.setup {
   defaults = {
