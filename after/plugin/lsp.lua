c1c240cf574be8a516fd653e963c70420d3c6cd4
Simone Colaci <simone.colaci@nl.abnamro.com>
2024-01-05 11:31:00 +0100

    changed config for volar

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,36 @@
+require("mason").setup()
+
+require("lsp-format").setup {}
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Automatic Server Setup :h mason-lspconfig-automatic-server-setup
+local handlers = {
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {
+      capabilities = capabilities,
+      on_attach    = function(client, bufnr)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+        vim.keymap.set('n', 'gi', require('telescope.builtin').lsp_references, {})
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+
+        if server_name == "volar" then
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            command = 'silent! EslintFixAll',
+          })
+        else
+          require("lsp-format").on_attach(client, bufnr)
+        end
+      end,
+    }
+  end,
+}
+
+require("mason-lspconfig").setup({ automatic_installation = true, handlers = handlers })
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,6 +4,7 @@ vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
 vim.opt.relativenumber = true
+vim.opt.number = true
 
 -- use spaces for tabs and whatnot
 vim.opt.tabstop = 2
