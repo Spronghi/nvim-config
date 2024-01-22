82503388d6a976425f21b1525e15caeb3cdbefa1
Simone Colaci <sim.colaci@gmail.com>
2024-01-22 20:58:51 +0100

    feat: luasnip

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,5 +1,33 @@
 return {
-  "L3MON4D3/LuaSnip",
   "saadparwaiz1/cmp_luasnip",
   "rafamadriz/friendly-snippets",
+  {
+    "L3MON4D3/LuaSnip",
+    configs = function()
+      local cmp = require("cmp")
+
+      -- TODO understand if these are needed or add a comment
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-o>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+          },
+          {
+            { name = 'buffer' },
+          }),
+      })
+    end
+  }
 }
