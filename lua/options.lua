cd8309dab79adc111474d6b429f69f3c3e64b2f8
Simone Colaci <simone.colaci@nl.abnamro.com>
2024-01-23 11:42:28 +0100

    feat: improved ctrl + o and ctrl + i

--- lua/options.lua
+++ lua/options.lua
@@ -22,3 +22,18 @@ vim.opt.expandtab = true
 
 -- share clipboard with yield
 vim.opt.clipboard = "unnamed"
+
+-- if you move more then 2 lines, it will add a jump so ctrl + o and ctrl + i are more resilient
+vim.keymap.set(
+  'n',
+  'j',
+  [[v:count ? (v:count >= 3 ? "m'" . v:count : '') . 'j' : 'gj']],
+  { noremap = true, expr = true }
+)
+
+vim.keymap.set(
+  'n',
+  'k',
+  [[v:count ? (v:count >= 3 ? "m'" . v:count : '') . 'k' : 'gk']],
+  { noremap = true, expr = true }
+)
