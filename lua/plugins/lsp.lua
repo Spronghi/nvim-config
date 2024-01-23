168cef93eb811e20746b48f719138a101c973d6d
Simone Colaci <sim.colaci@gmail.com>
2024-01-23 12:51:27 +0100

    fix: fixed snippets

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,51 +1,53 @@
 return {
-  { "williamboman/mason.nvim" },
-  { "lukas-reineke/lsp-format.nvim" },
-  { "neovim/nvim-lspconfig" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    keys = {
-      { "K",  vim.lsp.buf.hover,                           desc = "Hover infos" },
-      { "gd", vim.lsp.buf.definition,                      desc = "Go to definition" },
-      { "gD", vim.lsp.buf.declaration,                     desc = "Go to declaration" },
-      { "gr", require("telescope.builtin").lsp_references, desc = "Show references" },
-      { "ca", vim.lsp.buf.code_action,                     desc = "Code Action" },
-      { "rn", vim.lsp.buf.rename,                          desc = "Rename" },
-    },
-    config = function()
-      require("mason").setup()
-      require("lsp-format").setup()
-
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      -- Automatic Server Setup :h mason-lspconfig-automatic-server-setup
-      local handlers = {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {
-            capabilities = capabilities,
-            on_attach    = function(client, bufnr)
-              if server_name == "volar" then
-                vim.api.nvim_create_autocmd("BufWritePost", {
-                  command = 'silent! EslintFixAll',
-                })
-              else
-                require("lsp-format").on_attach(client, bufnr)
-              end
-            end,
-          }
-        end,
-      }
-
-      require("mason-lspconfig").setup({
-        automatic_installation = true,
-        handlers = handlers
-      })
-    end
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/vim-vsnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "lukas-reineke/lsp-format.nvim",
+    "neovim/nvim-lspconfig"
   },
+  lazy = false,
+  keys = {
+    { "K",  vim.lsp.buf.hover,                           desc = "Hover infos" },
+    { "gd", vim.lsp.buf.definition,                      desc = "Go to definition" },
+    { "gD", vim.lsp.buf.declaration,                     desc = "Go to declaration" },
+    { "gr", require("telescope.builtin").lsp_references, desc = "Show references" },
+    { "ca", vim.lsp.buf.code_action,                     desc = "Code Action" },
+    { "rn", vim.lsp.buf.rename,                          desc = "Rename" },
+  },
+  config = function()
+    require("mason").setup()
+    require("lsp-format").setup()
+
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    -- Automatic Server Setup :h mason-lspconfig-automatic-server-setup
+    local handlers = {
+      -- The first entry (without a key) will be the default handler
+      -- and will be called for each installed server that doesn't have
+      -- a dedicated handler.
+      function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup({
+          capabilities = capabilities,
+          on_attach = function(client, bufnr)
+            if server_name == "volar" then
+              vim.api.nvim_create_autocmd("BufWritePost", {
+                command = 'silent! EslintFixAll',
+              })
+            else
+              require("lsp-format").on_attach(client, bufnr)
+            end
+          end,
+        }
+        )
+      end,
+    }
+
+    require("mason-lspconfig").setup({
+      automatic_installation = true,
+      handlers = handlers
+    })
+  end
 }
--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,33 +1,33 @@
+-- refer to https://github.com/hrsh7th/nvim-cmp/ for docs
 return {
-  "saadparwaiz1/cmp_luasnip",
-  "rafamadriz/friendly-snippets",
-  {
-    "L3MON4D3/LuaSnip",
-    configs = function()
-      local cmp = require("cmp")
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/vim-vsnip",
+  },
+  config = function()
+    local cmp = require("cmp")
 
-      -- TODO understand if these are needed or add a comment
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-o>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
+    -- TODO understand if these are needed or add a comment
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-o>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      }),
+      snippet = {
+        expand = function(args)
+          vim.fn["vsnip#anonymous"](args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
         },
-        sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-          },
-          {
-            { name = 'buffer' },
-          }),
-      })
-    end
-  }
+        {
+          { name = 'buffer' },
+        }),
+    })
+  end
 }
