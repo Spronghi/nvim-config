d327876bc34ad948e31bfb0dc8e54a03f4ccfdbd
Simone Colaci <simone.colaci@nl.abnamro.com>
2024-01-24 23:50:26 +0100

    feat: changed lazymode

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,12 +1,13 @@
 return {
   "tpope/vim-fugitive",
   dependencies = { "nvim-telescope/telescope.nvim" },
+  event = "VeryLazy",
   keys = {
-    { "<leader>gg", vim.cmd.Git,                              desc = "Git" },
-    { "<laeder>gf", "<cmd>Git diff<cr>",                      desc = "Git diff" },
-    { "<leader>gd", vim.cmd.Gdiff,                            desc = "Gdiff" },
-    { "<leader>ga", "<cmd>Git add .<cr>",                     desc = "Git add ." },
-    { "<leader>gp", "<cmd>Git push<cr>",                      desc = "Git push" },
+    { "<leader>gg", vim.cmd.Git,          desc = "Git" },
+    { "<laeder>gf", "<cmd>Git diff<cr>",  desc = "Git diff" },
+    { "<leader>gd", vim.cmd.Gdiff,        desc = "Gdiff" },
+    { "<leader>ga", "<cmd>Git add .<cr>", desc = "Git add ." },
+    { "<leader>gp", "<cmd>Git push<cr>",  desc = "Git push" },
     {
       "<leader>gc",
       function()
