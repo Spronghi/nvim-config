d05e26b2201239f51b5a928c598e6fb0f5cf4d8c
Simone Colaci <simone.colaci@nl.abnamro.com>
2024-01-25 16:22:18 +0100

    feat: added vmux commands and git history

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -8,13 +8,28 @@ return {
     { "<leader>gd", vim.cmd.Gdiff,        desc = "Gdiff" },
     { "<leader>ga", "<cmd>Git add .<cr>", desc = "Git add ." },
     { "<leader>gp", "<cmd>Git push<cr>",  desc = "Git push" },
+    { "<leader>gh", "<cmd>Gllog<cr>",     desc = "Git history" },
+    {
+      "<leader>gb",
+      function()
+        local branch = vim.fn.input("Branch name: ")
+        local command = string.format('Git checkout -b %s', branch)
+
+        if branch == "" then
+          return
+        end
+
+        vim.cmd(command)
+      end,
+      desc = "Git checkout -b"
+    },
     {
       "<leader>gc",
       function()
         local message = vim.fn.input("Commit message: ")
         local command = string.format('Git commit -m"%s"', message)
 
-        if command == "" then
+        if message == "" then
           return
         end
 
