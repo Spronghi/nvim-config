da7901f453080fb8647225e9661deefeb956736b
Simone Colaci <simone.colaci@nl.abnamro.com>
2024-02-01 14:11:02 +0100

    feat: removed tmux configs

--- lua/plugins/vimux.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "preservim/vimux",
-  keys = {
-    { "<leader>vr", ":VimuxPromptCommand<cr>", desc = "Run on tmux" },
-    { "<leader>vi", ":VimuxInspectRunner<cr>", desc = "Inspect tmux runner" },
-    { "<leader>vz", ":VimuxZoomRunner<cr>",    desc = "Zoom tmux runner" },
-    { "<leader>vo", ":VimuxOpenRunner<cr>",    desc = "Open tmux runner" },
-  }
-}
