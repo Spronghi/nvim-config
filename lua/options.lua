c3df84c84e9bcf60eb4e5c582b4f5b5282ff2786
Simone Colaci <sim.colaci@gmail.com>
2024-02-03 16:11:14 +0100

    fix: changed tabs in windows

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,3 @@
-local utils = require("utils")
-
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
 vim.opt.autowrite = true
@@ -8,14 +6,8 @@ vim.opt.autoread = true
 vim.opt.relativenumber = true
 vim.opt.number = true
 
--- use 2 space tabs if you are not in linux and if you are editing the .config/nvim
-if utils.is_nvim_config() or not utils.is_linux() then
-  vim.opt.tabstop = 2
-  vim.opt.shiftwidth = 2
-else
-  vim.opt.tabstop = 4
-  vim.opt.shiftwidth = 4
-end
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 vim.opt.shiftround = true
 vim.opt.expandtab = true
